59d449572dda4245557b870817bedace78ffdb74
Fabien Boucher <fboucher@redhat.com>
2020-11-09 08:50:33 +0000

    Add basic game structure

--- oclogic.ml
+++ oclogic.ml
@@ -6,18 +6,38 @@ module Oclogic = struct
     printf "Calcule: %s = " (Compute.display c1);
     read_int ()
 
-  let round =
-    let create_compute op nl nr =
-      Compute.create (Number.from_int nl) (Number.from_int nr) op
-    in
+  let create_compute op =
+    Random.self_init ();
+    let nl = Random.int 10 in
+    let nr = Random.int 10 in
+    Compute.create (Number.from_int nl) (Number.from_int nr) op
 
-    let c1 = create_compute Compute.ADD 1 3 in
+  let round turn =
+    printf "Running turn %i\n" turn;
+    let c1 = create_compute Compute.ADD in
     let typed = display_and_read c1 in
 
     let expected = Compute.resolv c1 in
-    match (typed == expected) with
-      | true -> printf "Bravo !\n"
-      | false -> printf "Faux, essaie encore\n"
+    match typed == expected with
+    | true ->
+        printf "Bravo !\n";
+        true
+    | false ->
+        printf "Faux !\n";
+        false
+
+  let rec game turn turn_played =
+    printf "Running game with %i turns\n" turn;
+    printf "Turn played %i\n" turn_played;
+
+    let remain = turn - turn_played in
+    match remain with
+    | 0 -> ()
+    | _ ->
+        let _ = round turn_played in
+        game turn (turn_played + 1)
 end
 
-let () = Oclogic.round
+let () =
+  printf "Start game !\n";
+  Oclogic.game 3 0
